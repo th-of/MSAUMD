f53f0b560bb8a4d2b99120b5fbd132ae502bc58e
th-of <thof@nmbu.no>
2020-04-09 23:26:50 +0200

    Generalized version for calling from shell

--- SysSize.R
+++ SysSize.R
@@ -1,8 +1,11 @@
 # Calculate size of system
 # Thomas Oftedal (thof@nmbu.no)
+syssize <- function(){					
+args <- commandArgs(trailingOnly = TRUE)
 
 library("Rpdb")
-struc <- read.pdb("bilayer_GakA.pdb", ATOM = TRUE)
+struc <- read.pdb(args[1], ATOM = TRUE)
 newdata <- struc$atoms[which(struc$atoms$resname == 'WAT'), 9:11]
 cat(sprintf("%.6f %.6f %.6f", max(newdata$x1)-min(newdata$x1), max(newdata$x2)-min(newdata$x2), max(newdata$x3)-min(newdata$x3)))
-
+}
+syssize()
