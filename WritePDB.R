ba30527924ee4032b19d99d2c2e94b95fc547853
th-of <thof@nmbu.no>
2020-04-11 17:45:31 +0200

    Merge command and generalizing remain

--- WritePDB.R
+++ WritePDB.R
@@ -12,15 +12,51 @@ for (i in list.files(path = ".", pattern = "[A-Z]{1}.pdb")){
 redraw <- function(...){
   a <- Txyz(A.pdb, x = svalue(xval))
   aggr <- merge(a, B.pdb)
-  return(visualize(aggr, type = "l"))
+  return(c(rgl.clear(type = "all"), visualize(aggr, add = TRUE)))
 }
 
-window <- gwindow("Translation along the x-axis", visible=FALSE)
-group <- ggroup(cont = window, expand = TRUE)
+redraw_y <- function(...){
+  a <- Txyz(A.pdb, y = svalue(yval))
+  aggr <- merge(a, B.pdb)
+  return(c(rgl.clear(type = "all"), visualize(aggr, add = TRUE)))
+}
+
+redraw_z <- function(...){
+  a <- Txyz(A.pdb, z = svalue(zval))
+  aggr <- merge(a, B.pdb)
+  return(c(rgl.clear(type = "all"), visualize(aggr, add = TRUE)))
+}
+
+z_view <- function(...){
+  return(view3d(theta = 0, phi = 0))
+}
+x_view <- function(...){
+  return(view3d(theta = 90, phi = 0))
+}
+y_view <- function(...){
+  return(view3d(theta = 0, phi = 90))
+}
+
+window <- gwindow("Translation", visible=FALSE)
+group <- ggroup(horizontal = FALSE, cont = window, expand = TRUE)
+
 label <- glabel("x-axis", cont = group)
 xval <- gslider(from=-100, to=100, by=5, value=0, expand=TRUE, container = group, handler = redraw)
-visible(window) <- TRUE
 
+label <- glabel("y-axis", cont = group)
+yval <- gslider(from=-100, to=100, by=5, value=0, expand=TRUE, container = group, handler = redraw_y)
+
+label <- glabel("z-axis", cont = group)
+zval <- gslider(from=-100, to=100, by=5, value=0, expand=TRUE, container = group, handler = redraw_z)
+
+z_set <- gbutton(text = "Z view", border = TRUE, container = group, handler = z_view)
+x_set <- gbutton(text = "X view", border = TRUE, container = group, handler = x_view)
+y_set <- gbutton(text = "Y view", border = TRUE, container = group, handler = y_view)
+
+act <- gaction("merge", handler=function(...) {print(svalue(xval))})
+b3 <- gbutton(action=act, cont=group)
+
+visible(window) <- TRUE
 
 # gakb[["atoms"]][["chainid"]] <- "B"
 # 
