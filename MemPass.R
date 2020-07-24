f03cdb84868457cad77e9068aa17f73cadcad162
Thomas <thomas@pop-os.localdomain>
2020-07-24 14:50:52 +0200

    find water molecules crossing the membrane

---
+++ MemPass.R
@@ -0,0 +1,46 @@
+## Mempass
+# cpptraj
+# parm prmtop
+# trajin nc
+# trajin filename.pdb pdb multi start x stop y
+# https://math.stackexchange.com/questions/1472049/check-if-a-point-is-inside-a-rectangular-shaped-area-3d
+library("bio3d")
+
+files <- list.files("./pdbs")
+files <- files[order(nchar(files), files)]
+l <- vector("list", length(files))
+
+## Calculate the vertices of a rectangle formed by the membrane lipids (from the first pdb only)
+a <- read.pdb(paste0("./pdbs/", files[1]), ATOM.only = TRUE)
+frame <- as.data.frame(a$atom)
+frame2 <- frame[frame$resid == 'OPE' | frame$resid == 'OPG', ]
+
+x_min <- min(frame2$x)
+x_max <- max(frame2$x)
+
+y_min <- min(frame2$y)
+y_max <- max(frame2$y)
+
+z_min <- min(frame2$z)
+z_max <- max(frame2$z)
+
+P_1 <- c(x_max, y_min, z_min)
+P_2 <- c(x_max, y_max, z_min)
+P_3 <- c(x_min, y_max, z_min)
+P_4 <- c(x_min, y_min, z_min)
+
+P_5 <- c(x_max, y_min, z_max)
+P_6 <- c(x_max, y_max, z_max)
+P_7 <- c(x_min, y_max, z_max)
+P_8 <- c(x_min, y_min, z_max)
+
+
+for (i in numfils){
+  a <- read.pdb(paste0("./pdbs/", files[i]), ATOM.only = TRUE)
+  frame <- as.data.frame(a$atom)
+  frame <- frame[which(frame$resid == 'WAT'),]
+  l[[i]] <- frame
+}
+
+
+
